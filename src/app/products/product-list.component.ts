415bec35d124e46607c53619e3ee308cbb4c9c67
Carlos Carrero <carlos.carrero@gmail.com>
2019-07-08 19:46:43 -0500

    using ViewChild to access a child component

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -2,15 +2,20 @@ import { Component, OnInit, ViewChild, AfterViewInit, ElementRef, ViewChildren, 
 
 import { IProduct } from './product';
 import { ProductService } from './product.service';
+import { CriteriaComponent } from '../shared/criteria/criteria.component';
 
 @Component({
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, AfterViewInit {
+
     pageTitle: string = 'Product List';
     showImage: boolean;
     includeDetail: boolean = true;
+
+    @ViewChild(CriteriaComponent) filterComponent: CriteriaComponent;
+    parentListFilter: string;
 
     imageWidth: number = 50;
     imageMargin: number = 2;
@@ -25,10 +30,14 @@ export class ProductListComponent implements OnInit {
         this.productService.getProducts().subscribe(
             (products: IProduct[]) => {
                 this.products = products;
-                this.performFilter();
+                this.performFilter(this.parentListFilter);
             },
             (error: any) => this.errorMessage = <any>error
         );
+    }
+
+    ngAfterViewInit(): void {
+      this.parentListFilter = this.filterComponent.listFilter;
     }
 
     toggleImage(): void {
