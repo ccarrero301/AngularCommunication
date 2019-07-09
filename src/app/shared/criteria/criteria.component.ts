7cce2d21b9f0bade535c43c341daa9811c270e2e
Carlos Carrero <carlos.carrero@gmail.com>
2019-07-08 19:26:43 -0500

    Using onChanges to watch for changes in a child component

--- src/app/shared/criteria/criteria.component.ts
+++ src/app/shared/criteria/criteria.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'pm-criteria',
   templateUrl: './criteria.component.html',
   styleUrls: ['./criteria.component.css']
 })
-export class CriteriaComponent implements OnInit, AfterViewInit {
+export class CriteriaComponent implements OnInit, AfterViewInit, OnChanges {
 
   listFilter: string;
+  hitMessage: string;
+
   @Input() displayDetail: boolean;
   @Input() hitCount: number;
 
@@ -21,6 +23,14 @@ export class CriteriaComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     if (this.filterElementRef.nativeElement) {
       this.filterElementRef.nativeElement.focus();
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.hitCount && !changes.hitCount.currentValue) {
+      this.hitMessage = 'No matches found';
+    } else {
+      this.hitMessage = 'Hits: ' + this.hitCount;
     }
   }
 
